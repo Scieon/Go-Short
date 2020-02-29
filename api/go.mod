624e3b18fba9786dec92eab18e832d3710f3e214
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@gmail.com>
2020-02-29 13:34:06 -0500

    Setup with go mod

---
+++ api/go.mod
@@ -0,0 +1,9 @@
+module playground/Go-Short/api
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-redis/redis/v7 v7.2.0
+	go.uber.org/zap v1.14.0
+)
---
+++ api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger, _ := zap.NewDevelopment()
+	sugar := logger.Sugar()
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	sugar.Info("Starting server")
+	r.Run()
+}
